31d80b0102a359a74fb217fe9feb1aebf33dcc22
Bachan Kubi <felixghagra@gmail.com>
2025-02-26 18:37:19 +0600

    deep learning on js concept

---
+++ javascript-fundamentals/fetch/index.js
@@ -0,0 +1,20 @@
+// const data = fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res=>res.json())
+//     .then(data=>{
+//         data.map(t=>console.log(t.name))
+//     })
+
+// const names = document.getElementById("names");
+// console.log(names);
+// async function fetchData() {
+//     try {
+//         const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await res.json();
+//         names.innerHTML = data.map(n=>`<p>${n.name}</p>`).join('');
+
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
+// fetchData();
+
