ea9acbba5ef261de34c3f7f4ef2466c4300d80b6
Bachan Kubi <felixghagra@gmail.com>
2025-03-11 11:10:58 +0600

    deep learning on js concept

--- javascript-fundamentals/loops/loops.js
+++ javascript-fundamentals/loops/loops.js
@@ -1,26 +1,54 @@
 // while loop
-let i = 2;
-while(i<5){
-    console.log('i is zero');
-    i++;
-}
-document.getElementById("start-game").addEventListener("click", function () {
-    const targetNumber = Math.floor(Math.random() * 100) + 1;
-    let guess = null;
-    let attempts = 0;
+// let i = 2;
+// while(i<5){
+//     console.log('i is zero');
+//     i++;
+// }
+// document.getElementById("start-game").addEventListener("click", function () {
+//     const targetNumber = Math.floor(Math.random() * 100) + 1;
+//     let guess = null;
+//     let attempts = 0;
     
-    while (guess !== targetNumber) {
-      guess = parseInt(prompt("Enter your guess (1-100):"));
-      attempts++;
+//     while (guess !== targetNumber) {
+//       guess = parseInt(prompt("Enter your guess (1-100):"));
+//       attempts++;
 
-      if (guess === targetNumber) {
-        document.getElementById("message").innerText = `🎉 Correct! You guessed it in ${attempts} attempts.`;
-      } else if (guess > targetNumber) {
-        alert("Too high! Try again.");
-      } else if (guess < targetNumber) {
-        alert("Too low! Try again.");
-      } else {
-        alert("Invalid input. Please enter a number.");
-      }
-    }
-  });
+//       if (guess === targetNumber) {
+//         document.getElementById("message").innerText = `🎉 Correct! You guessed it in ${attempts} attempts.`;
+//       } else if (guess > targetNumber) {
+//         alert("Too high! Try again.");
+//       } else if (guess < targetNumber) {
+//         alert("Too low! Try again.");
+//       } else {
+//         alert("Invalid input. Please enter a number.");
+//       }
+//     }
+//   });
+
+  // let p = 5;
+  // while(p<10){
+  //   p++;
+  //   console.log("increment", p);
+  // };
+
+  // let c = 5;
+  // while(c>0){
+  //   c--;
+  //   console.log("derement", c);
+  // }
+
+// do while loop- The loop will firt execute the body then check the conditons. 
+
+
+let i = 2;
+do{
+  i++;
+  console.log(i)
+}while(i<5);
+
+let p = 5;
+do{
+  p--;
+  console.log(p);
+}while(p>1);
+
