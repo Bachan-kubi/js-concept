f047952ad1417f541f8ac3737b610a2751a531cb
Bachan Kubi <felixghagra@gmail.com>
2025-03-17 20:59:04 +0600

    deep learning on js concept

--- w3resources/DOM/backgroundColor.js
+++ w3resources/DOM/backgroundColor.js
@@ -1,30 +1,38 @@
-function changeBgColor(){
-    const color = document.getElementById('bg-color');
-    color.classList.add('b-color');
-}
+// function changeBgColor(){
+//     const color = document.getElementById('bg-color');
+//     color.classList.add('b-color');
+// }
 
-const pageTitle = document.getElementById('page-title');
-console.log(pageTitle)
-const classNames = document.getElementsByClassName('family');
-console.log(classNames[1]);
-// const arr = classNames.forEach(ele=>console.log(ele))
+// const pageTitle = document.getElementById('page-title');
+// console.log(pageTitle)
+// const classNames = document.getElementsByClassName('family');
+// console.log(classNames[1]);
+// // const arr = classNames.forEach(ele=>console.log(ele))
 
-const convertedToArray = [...classNames];
-console.log(convertedToArray)
-convertedToArray.forEach(ele=>console.log(ele))
+// const convertedToArray = [...classNames];
+// console.log(convertedToArray)
+// convertedToArray.forEach(ele=>console.log(ele))
 
-const tags = document.getElementsByTagName('p');
-console.log(tags)
-const allTags = [...tags];
-allTags.map(elem=>console.log(elem));
+// const tags = document.getElementsByTagName('p');
+// console.log(tags)
+// const allTags = [...tags];
+// allTags.map(elem=>console.log(elem));
 
-// const querySelectors = document.querySelectorAll('section button:nth-child(3)');
-const querySelectors = document.querySelectorAll('section button');
-console.log(querySelectors)
-querySelectors.forEach(ele=>console.log(ele));
-// change elements
-const changeContentJs = document.getElementById('js');
-const changeContentPara = document.getElementById('para');
-console.log(changeContentJs);
-changeContentJs.innerHTML= '<h1>ami change korte <b>parsi</b></h1>';
-changeContentJs.classList.add('c-color');
+// // const querySelectors = document.querySelectorAll('section button:nth-child(3)');
+// const querySelectors = document.querySelectorAll('section button');
+// console.log(querySelectors)
+// querySelectors.forEach(ele=>console.log(ele));
+// // change elements
+// const changeContentJs = document.getElementById('js');
+// const changeContentPara = document.getElementById('para');
+// console.log(changeContentJs);
+// changeContentJs.innerHTML= '<h1>ami change korte <b>parsi</b></h1>';
+// changeContentJs.classList.add('c-color');
+// // text content
+const p = document.querySelector('p');
+console.log(p.innerText)
+console.log(p.textContent)
+
+const footer = document.getElementById('footer');
+console.log(footer.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+console.log(footer)
--- w3resources/DOM/script.js
+++ w3resources/DOM/script.js
@@ -51,20 +51,37 @@ console.log(para, button);
 // }
 
 // get form value
-function getFormValue(event) {
-    event.preventDefault();
-    let fname = document.getElementById('fname');
-    console.dir(fname.value);
-    // method-1
-    // const v = document.getElementById('form-1');
-    // console.dir(v.elements['fname'].value);
-    // console.dir(v.elements['lname'].value);
+// function getFormValue(event) {
+//     event.preventDefault();
+//     let fname = document.getElementById('fname');
+//     console.dir(fname.value);
+//     // method-1
+//     // const v = document.getElementById('form-1');
+//     // console.dir(v.elements['fname'].value);
+//     // console.dir(v.elements['lname'].value);
     
-    // method-2
-    // for (let i = 0; i < v.elements.length; i++) {
-    //     let element = v.elements[i];
-    //     if (element.type !== "button" && element.type !== "submit") {
-    //         console.log(element.value);
-    //     }
-    // }
-}
+//     // method-2
+//     // for (let i = 0; i < v.elements.length; i++) {
+//     //     let element = v.elements[i];
+//     //     if (element.type !== "button" && element.type !== "submit") {
+//     //         console.log(element.value);
+//     //     }
+//     // }
+// }
+const ex = document.getElementById('ex')
+console.log(ex)
+
+console.log(ex.offsetParent.id); // main
+console.log(ex.offsetLeft); // 180 (note: a number, not a string "180px")
+console.log(ex.offsetTop); 
+
+// local storage 
+localStorage.setItem('test', 1);
+console.log(localStorage.getItem('test'));
+const re= localStorage.removeItem('test');
+console.log(re);
+const newStore = localStorage.setItem('ami', 'kkk')
+const res = localStorage.clear('ami')
+sessionStorage.setItem('nai', 1);
+
+
