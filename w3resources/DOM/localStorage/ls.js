50a021e42727fc4aa07e9d28ed4f260f1df4a8bc
Bachan Kubi <felixghagra@gmail.com>
2025-03-25 13:32:55 +0600

    deep learning on js concept

--- w3resources/DOM/localStorage/ls.js
+++ w3resources/DOM/localStorage/ls.js
@@ -1,22 +1,22 @@
-// initialize pages 
-const textArea = document.getElementById('myTextArea');
-function initializeTextArea(){
-    console.log(textArea);
-    
-    // checkde whether saved in local storage
-    if(localStorage.getItem('savedText')){
-        textArea.value = localStorage.getItem('savedText');
+// initialize pages
+const textArea = document.getElementById("myTextArea");
+
+function initializeText(){
+    if(localStorage.getItem('savedTxt')){
+        textArea.value = localStorage.getItem('savedTxt')
     }
+
     textArea.addEventListener('input', saveText);
 }
-// save text to local storage
+
 function saveText(e){
     const currentText = e.target.value;
-    localStorage.setItem('savedText', currentText);
+    localStorage.setItem('savedTxt', currentText);
 }
 function clearText(){
-    localStorage.removeItem('savedText');
+    localStorage.removeItem('savedTxt');
     textArea.value = "";
 }
 
-window.onload = initializeTextArea;
+window.onload = initializeText;
+
