778a85d8a7b778017efbda7faa509421e176b930
Bachan Kubi <felixghagra@gmail.com>
2025-04-24 11:40:49 +0600

    js basics learning

--- w3resources/DOM/DOM-Basics/getA.js
+++ w3resources/DOM/DOM-Basics/getA.js
@@ -12,13 +12,23 @@ function getAttr() {
 
   // get form value
   const form = document.getElementById('form1');
-  console.log(form.elements);
+  // console.log(form.elements);
   
   function getFormValue(e){
     e.preventDefault();
+    const form = e.target;
     const firstName = form.elements['firstname'].value;
     const lastName = form.elements['lastname'].value;
     const email = form.elements['email'].value;
     console.log(firstName, lastName, email);
+  }
+
+  // form 2
+  const form2 = document.getElementById("form2");
+  form2.addEventListener('change', formTwo);
+  function formTwo(e){
+    console.log(e);
+    const inputName = e.target.id
+    console.log(inputName);
   }
   
