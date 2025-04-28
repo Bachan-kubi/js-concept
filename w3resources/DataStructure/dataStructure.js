e328a2b7c9f2cfa19f707b1f7b8b6c4cabd52cbb
Bachan Kubi <felixghagra@gmail.com>
2025-04-28 23:04:44 +0600

    data structure

--- w3resources/DataStructure/dataStructure.js
+++ w3resources/DataStructure/dataStructure.js
@@ -18,3 +18,26 @@ queue.push('b')
 queue.push('c')
 queue.push('d')
 console.log(queue.unshift(10));
+
+const arr = ['1', 2, 3, 4, 50];
+const [fisr, s, t, ...a] = arr;
+t;
+s
+a
+console.log(arr.indexOf(2, ));
+console.log(arr.lastIndexOf(50, ));
+console.log(arr.indexOf(2, ));
+console.log(arr.findIndex(item=>item===2));
+console.log(arr.find(val=>val===2));
+const people = [
+    {name: 'kubi'},
+    {name: 'robi'}
+];
+people.find(m=>m.name === 'kubi');
+people
+people;
+people.name = 'ami';
+people;
+m.name= 'komu';
+m;
+people
