68a43e46dd6e65bef0367b181461e1a2e1c50fd2
Bachan Kubi <felixghagra@gmail.com>
2025-05-05 08:36:53 +0600

    learning class

---
+++ javascript-fundamentals/class/clock.js
@@ -0,0 +1,33 @@
+class Clock{
+    constructor({template}){
+        this.template = template;
+    }
+    render(){
+        const date = new Date();
+        let hours = date.getHours();
+        if(hours<10){
+            hours= "0"+ hours
+        }
+        let minutes = date.getMinutes();
+        if(minutes<10) minutes = "0"+ minutes;
+        let seconds = date.getSeconds();
+        if(seconds<10) seconds = "0"+ seconds;
+        let output = this.template
+                    .replace('H', hours)
+                    .replace('M', minutes)
+                    .replace('S', seconds)
+        console.log(output);
+    }
+    stop(){
+        clearInterval(this.timer);
+    }
+    start(){
+        this.render();
+        this.timer = setInterval(()=>this.render(), 1000)
+    }
+}
+
+const c = new Clock({template: "h:m:s"});
+c.start()
+
+
