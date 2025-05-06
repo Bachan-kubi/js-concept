d81a621d1c44001367be65c3bead8ca4705f8461
Bachan Kubi <felixghagra@gmail.com>
2025-05-06 13:24:19 +0600

    class method learnt

--- javascript-fundamentals/class/class.js
+++ javascript-fundamentals/class/class.js
@@ -25,16 +25,31 @@ class Car{
         this.year = year
     };
     age(x){
-        // let date = new Date()
-        // return date.getFullYear()-this.year;
-        return x-this.year;
-        
+        return x-this.year; 
     };
+    stop(s){
+        return(
+            `${s} should stop `)
+        
+    }
 }
 // const car = new Car("ford", 2000);
 const car1 = new Car("Toyota", 2005);
 let date = new Date();
 let year = date.getFullYear();
-// document.getElementById('car').textContent = `Car name is: ` + car1.name + ", " + "Age: "+car1.age(year);
+document.getElementById('car').textContent = `Car name is: ` + car1.name + ", " + "Age: "+car1.age(year);
 console.log('object');
+
+// extends key words
+class Cycle extends Car{
+    break(){
+        console.log(`${this.name} breaks hardly and its making year is ${this.year}`);
+    }
+}
+const bike = new Cycle("My Bike", 2001);
+console.log(bike);
+bike.break();
+console.log(bike.stop('my bike'));
+
+
 
