c750bacb29049c32f1fe91791e20e502c7c8baa9
Bachan Kubi <felixghagra@gmail.com>
2025-05-11 19:55:56 +0600

    learning prototype inheritance

---
+++ javascript-fundamentals/prototypeInheritance/main.js
@@ -0,0 +1,27 @@
+let animal = {
+    eats: true,
+    run(){
+        console.log(`animal runs`);
+    },
+    play(){
+        console.log('animal play');
+    }
+};
+let rabbit = {
+    jumps: true,
+    hide(){
+        console.log('it can hide in jungle');
+    },
+    __proto__:animal,
+};
+rabbit.play  = function(){
+    console.log('rabits plays');
+}
+let earLong = {
+    earLength :10,
+    __proto__: rabbit
+};
+rabbit.play();
+console.log(earLong);
+rabbit.run();
+console.log(rabbit);;
