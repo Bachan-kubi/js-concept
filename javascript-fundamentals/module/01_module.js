c2f1fd59dacbaffb9f1a6f7aa896041a434bd70c
Bachan Kubi <felixghagra@gmail.com>
2025-05-27 16:04:39 +0600

    module

---
+++ javascript-fundamentals/module/01_module.js
@@ -0,0 +1,14 @@
+import { nissan } from "./02_module.js";
+
+function sayHi(){
+    console.log('sayHI');
+};
+
+function amiChai(n){
+    console.log(`${n}, i want you to know`);
+}
+export const model = nissan.aboutAll();
+export{sayHi, amiChai}
+
+
+
---
+++ javascript-fundamentals/module/module.js
@@ -0,0 +1,6 @@
+import { sayHi, amiChai, model } from "./01_module.js";
+sayHi();
+amiChai("Rimi");
+console.log(model);
+
+
