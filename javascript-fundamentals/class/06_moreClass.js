d434cad1541d819e3a14feede13f3879f4c6a57e
Bachan-kubi <felixghagra@gmail.com>
2025-07-07 20:34:10 +0600

    learning class

---
+++ javascript-fundamentals/class/06_moreClass.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(name, job, age, details){
+        this.name = name;
+        this.job = job;
+        this.age = age;
+        this.details = details;
+    }
+    drive(){
+        console.log(`${this.name} can drive car at ${this.age} after leaving his ${this.job} ${this.details}`);
+    }
+}
+const pitchi = {
+    name: "kubi",
+    job: 'curi',
+    age: 40,
+    details: 'living in birisiri, and get the world to get eneo'
+}
+const bachan = new User(pitchi.name, pitchi.job, pitchi.age, pitchi.details)
+bachan.drive()
