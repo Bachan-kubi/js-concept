d22e8f9184b3c81070cda0f2060e7616e74ad49d
Bachan-kubi <felixghagra@gmail.com>
2025-09-11 17:32:54 +0600

    array method splice is being practiced

--- javascript-fundamentals/arrayMethods/splice.js
+++ javascript-fundamentals/arrayMethods/splice.js
@@ -1,2 +1,6 @@
 const fruits = ['mangoo', 'apple', 'jackfruits',"pineapple"];
 
+// splice method- takes 3 parameters: 1st one for indexing arrays elements, 2nd numbers to be deleted, 3rd is elements to be replaced.
+fruits.splice(-1,0,"boss", 'ros');
+fruits.splice(2,4)
+console.log(fruits);
